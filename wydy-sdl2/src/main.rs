95eb8db3a2c56cc0f226fa44376d960b9a9cea78
babariviere <babariviere@protonmail.com>
2016-11-16 22:32:10 +0100

    Update sdl2 ttf version

--- wydy-sdl2/src/main.rs
+++ wydy-sdl2/src/main.rs
@@ -1,6 +1,40 @@
 extern crate sdl2;
 extern crate sdl2_ttf;
 
+use sdl2::pixels::Color;
+use sdl2::event::Event;
+use sdl2::keyboard::Keycode;
+
 fn main() {
-    println!("Hello, world!");
+    let context = sdl2::init().unwrap();
+    let video = context.video().unwrap();
+
+    let mut window = video.window("Wydy Client", 800, 600)
+        .borderless()
+        .fullscreen_desktop()
+        .opengl()
+        .build()
+        .unwrap();
+
+
+    let mut event_pump = context.event_pump().unwrap();
+    {
+        let mut surface = window.surface_mut(&event_pump).unwrap();
+        surface.set_alpha_mod(0);
+    }
+    let mut renderer = window.renderer().build().unwrap();
+
+    'running: loop {
+        renderer.set_draw_color(Color::RGBA(100, 100, 100, 0));
+        renderer.clear();
+        renderer.present();
+
+        for event in event_pump.poll_iter() {
+            match event {
+                Event::Quit { .. } |
+                Event::KeyDown { keycode: Some(Keycode::Escape), .. } => break 'running,
+                _ => {}
+            }
+        }
+    }
 }
